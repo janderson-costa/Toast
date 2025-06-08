b072e44a2ec15bf9abaea4e40784217ce50fe5c1
janderson-costa <jandersonfcosta@outlook.com>
2025-06-08 00:21:33 -0300

    ---

--- Toast copy.js
+++ Toast copy.js
@@ -35,7 +35,11 @@ export default function Toast(options = {}) {
 				</div>
 			</div>
 			<div class="toast-button">
-				<button type="button" class="toast-button-icon" onclick="this.parentElement.parentElement.remove()">✖</button>
+				<button type="button" class="toast-button-icon" onclick="this.parentElement.parentElement.remove()">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+						<path d="M18 6 6 18"></path><path d="m6 6 12 12"></path>
+					</svg>
+				</button>
 			</div>
 		`;
 
