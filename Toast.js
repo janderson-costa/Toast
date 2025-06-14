ab4192d71d7061acee69aa91f8113910961d2568
janderson-costa <jandersonfcosta@outlook.com>
2025-06-14 18:58:20 -0300

    ---

--- Toast.js
+++ Toast.js
@@ -71,7 +71,11 @@ export default function Toast(options = {}) {
 				</div>
 				<div class="toast-controls">
 					<div class="toast-button hidden">
-						<button type="button" class="toast-button-icon" title="Fechar">✖</button>
+						<button type="button" class="toast-button-icon" title="Fechar">
+							<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+							</svg>
+						</button>
 					</div>
 					<div class="toast-spin hidden"></div>
 				</div>
